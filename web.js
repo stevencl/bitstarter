4869172daa1d250a684b4e4632729bb83aef7dea
Steven Clarke <steven_clarke@hotmail.com>
2013-07-29 23:13:01 +0000

    Updated web.js to return hello world from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var s = fs.readFile("index.html").toString();
+  response.send(s);
 });
 
 var port = process.env.PORT || 5000;
